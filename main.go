9e896688618664b8e6e2748d566531bdc8b259e8
Dave Llewellyn <439837@gmail>
2016-10-18 08:39:25 +0100

    Final changes

--- main.go
+++ main.go
@@ -5,17 +5,13 @@ import (
 	"html/template"
 	"io/ioutil"
 	"net/http"
-	"errors"
 	"regexp"
 )
 
 const txtFileExt = "%v.txt"
-const htmlFileExt = "%v.html"
+const viewPath = "/view/"
+const editPath = "/edit/"
 
-var viewHandler WikiHandler
-var editHandler WikiHandler
-var saveHandler WikiHandler
-var errorHandler WikiHandler
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
@@ -24,84 +20,19 @@ type Page struct {
 	Body  []byte
 }
 
-type WikiHandler struct {
-	uri     string
-	handler http.HandlerFunc
-}
-
 func main() {
-	viewHandler = WikiHandler{uri: "/view/", handler: viewHandlerFunc}
-	editHandler = WikiHandler{uri: "/edit/", handler: editHandlerFunc}
-	saveHandler = WikiHandler{uri: "/save/", handler: saveHandlerFunc}
-	errorHandler = WikiHandler{uri: "/error", handler: errorHandlerFunc}
+	handlersMap := map[string]http.HandlerFunc{
+		"/view/": makeHandler(viewHandler),
+		"/edit/": makeHandler(editHandler),
+		"/save/": makeHandler(saveHandler),
+	}
 
-	registerHandler(viewHandler, editHandler, saveHandler, errorHandler)
+	for name, handler := range handlersMap {
+		fmt.Printf("\t[startup]Registering http.Handler: %v\n", name)
+		http.Handle(name, handler)
+	}
+
 	http.ListenAndServe(":8080", nil)
-}
-
-/* Register the Wiki handlers */
-func registerHandler(wikiHandlers ...WikiHandler) {
-	for _, handler := range wikiHandlers {
-		fmt.Printf("[startup] Registering http.Handler: %v\n", handler.name())
-		http.HandleFunc(handler.uri, handler.handler)
-	}
-}
-
-/* View the requested page. */
-func viewHandlerFunc(w http.ResponseWriter, r *http.Request) {
-	filename, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
-	p, err := loadPage(filename)
-	if err != nil {
-		fmt.Printf("[viewHandlerFunc] error loading file: %v\n", err.Error())
-		http.Redirect(w, r, editHandler.uri + filename, http.StatusFound)
-		return
-	}
-	renderTemplate(p, viewHandler, w)
-}
-
-func editHandlerFunc(w http.ResponseWriter, r *http.Request) {
-	filename, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
-	p, err := loadPage(filename)
-	if err != nil {
-		fmt.Printf("error loading file: %v\n", err.Error())
-		p = &Page{Title: filename}
-	}
-	renderTemplate(p, editHandler, w)
-}
-
-func saveHandlerFunc(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
-	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-	err2 := p.save()
-
-	if err2 != nil {
-		fmt.Printf("\n[saveHandlerFunc] Something went wrong %v\n", err2.Error())
-	}
-	http.Redirect(w, r, viewHandler.uri + title, http.StatusFound)
-}
-
-func errorHandlerFunc(w http.ResponseWriter, r *http.Request) {
-	err := errors.New("This aint working no more.")
-	http.Error(w, err.Error(), http.StatusInternalServerError)
-	return
-}
-
-func renderTemplate(p *Page, handler WikiHandler, w http.ResponseWriter) {
-	err := templates.ExecuteTemplate(w, handler.templateName(), p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
 }
 
 func (p *Page) save() error {
@@ -118,24 +49,54 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func (h *WikiHandler) templateName() string {
-	templatePath := fmt.Sprintf(htmlFileExt, h.name())
-	fmt.Printf("Template path for %v is %v\n", h.uri, templatePath)
-	return templatePath
+func makeHandler(fn func(w http.ResponseWriter, r *http.Request, title string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Inside make handler")
+		titleMatch := validPath.FindStringSubmatch(r.URL.Path)
+		if titleMatch == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, titleMatch[2])
+	}
 }
 
-func (h *WikiHandler) name() string {
-	if lastChar := string(h.uri[len(h.uri) - 1]); lastChar == "/" {
-		return h.uri[1 : len(h.uri) - 1]
+/* View the requested page. */
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	fmt.Println("view handler")
+	p, err := loadPage(title)
+	if err != nil {
+		fmt.Printf("[viewHandlerFunc] error loading file: %v\n", err.Error())
+		http.Redirect(w, r, editPath + title, http.StatusFound)
+		return
 	}
-	return h.uri[1:]
+	renderTemplate(p, "view.html", w)
 }
 
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-	m := validPath.FindStringSubmatch(r.URL.Path)
-	if m == nil {
-		http.NotFound(w, r)
-		return "", errors.New("Invalid Page Title")
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		fmt.Printf("error loading file: %v\n", err.Error())
+		p = &Page{Title: title}
 	}
-	return m[2], nil // The title is the second subexpression.
+	renderTemplate(p, "edit.html", w)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err2 := p.save()
+
+	if err2 != nil {
+		fmt.Printf("\n[saveHandlerFunc] Something went wrong %v\n", err2.Error())
+	}
+	http.Redirect(w, r, viewPath + title, http.StatusFound)
+}
+
+func renderTemplate(p *Page, templateName string, w http.ResponseWriter) {
+	err := templates.ExecuteTemplate(w, templateName, p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
